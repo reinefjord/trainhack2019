1f5411f49bada82bb5840185f6b4ad737d99b8ec
Tobias Skarhed <tobias.skarhed@gmail.com>
2019-08-15 23:48:33 +0200

    Add DataHandler and search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import FindTrain from "./components/FindTrain/FindTrain";
+import DataHandler from './components/DataHandler/DataHandler';
+import "./App.css";
+import Route from "./components/route/Route";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <DataHandler>
+            {({train, stops}) => (
+            <>
+              <FindTrain />
+              <span>{train}</span>
+
+              <Route stops={stops}/>
+              </>
+            )}
+          </DataHandler>
+        </header>
+      </div>
+    );
+  
 }
 
 export default App;
