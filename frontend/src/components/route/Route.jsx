6e2c46cd39b63b53269707d9bfbd78ce8d901f60
Oskar Segersvärd <victorystick@gmail.com>
2019-08-16 10:31:29 +0200

    Start on the real route_backend client

--- frontend/src/components/route/Route.jsx
+++ frontend/src/components/route/Route.jsx
@@ -1,57 +1,59 @@
 import React from 'react';
 import './Route.css';
+import { client } from '../../routes_backend/client.js';
 
 const time = new Intl.DateTimeFormat(undefined, {
   timeStyle: 'short',
 });
 
-function Stop({name, departureTime}) {
+function Stop(stop) {
   return (
     <div className="Stop" key="id">
       <div className="StopName">
-        <div>{name}</div>
-        <div>{time.format(departureTime)}</div>
+        <div>{stop.name}</div>
+        <div>{time.format(stop.departureTime)}</div>
       </div>
       <div className="Blob"></div>
-      <div className="AlternativeInfo">
-        {
-          // We'll put something interesting here at some point.
-        }
-      </div>
+      <AlternativeRouteInfo {...stop} />
     </div>
   );
 }
 
-export default class Route extends React.Component {
-  render() {
-    const {stops: [origin, ...stops]} = this.props;
-    return (
-      <div className="Route">
-        <Stop {...origin}></Stop>
-        {stops.map((stop) => <Stop key={stop.id} {...stop}/>)}
-      </div>
-    );
-  }
+export default function({stops: [origin, ...stops]}) {
+  return (
+    <div className="Route">
+      <Stop {...origin}></Stop>
+      {stops.map((stop) => <Stop key={stop.id} {...stop}/>)}
+    </div>
+  );
 }
 
-const exampleStops = [
-  {
-    id: 0,
-    name: 'Stockholm C',
-    departureTime: Date.now(),
-  },
-  {
-    id: 1,
-    name: 'Alvesta',
-    departureTime: Date.now() + 134531,
-  },
-  {
-    id: 2,
-    name: 'Malmo',
-    departureTime: Date.now() + 164513,
-  },
-];
+function alternatives(n) {
+  return `${n || 'no'} alternative${n === 1 ? '' : 's'}`;
+}
 
-export function DemoRoute() {
-  return <Route stops={exampleStops}></Route>;
+class AlternativeRouteInfo extends React.Component {
+  state = {
+    alternatives: null,
+    error: null,
+  };
+
+  componentDidMount() {
+    client.getAlternativeRoutes(this.props)
+      .then(
+        (alternatives) => this.setState({ alternatives }),
+        (error) => this.setState({ error }));
+  }
+
+  render() {
+    if (this.state.error) {
+      return <p>{String(this.state.error)}</p>
+    }
+
+    if (!this.state.alternatives) {
+      return <p>Loading...</p>;
+    }
+
+    return <p>{alternatives(this.state.alternatives.length)}</p>;
+  }
 }
