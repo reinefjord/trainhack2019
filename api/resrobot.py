b932daac6fb09d110d129a53e4afbd94a549858a
Anton Grensjö <grensjo@google.com>
2019-08-17 08:56:20 +0200

    Fix timezone bug.

--- api/resrobot.py
+++ api/resrobot.py
@@ -2,6 +2,7 @@ import config
 import requests
 from datetime import datetime
 from pprint import pprint
+from dateutil import tz
 
 def _do_get(endpoint, query):
     query['key'] = config.resrobot_reseplanerare
@@ -14,16 +15,21 @@ def _do_get(endpoint, query):
 
     return r.json()
 
-def search_routes(when: datetime, origin, dest):
+def search_routes(when_utc: datetime, origin, dest):
     """
     args:
         when - the earliest departure time to search for
         origin - pair of latitude and longitude of the origin location
         dest - pair of latitude and longitude of the destination location
     """
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz('Europe/Stockholm')
+    when_utc = when_utc.replace(tzinfo=from_zone)
+    when_local = central = when_utc.astimezone(to_zone)
+
     return _do_get('trip', {
-            'date': when.strftime('%Y-%m-%d'),
-            'time': when.strftime('%H:%M'),
+            'date': when_local.strftime('%Y-%m-%d'),
+            'time': when_local.strftime('%H:%M'),
             'originCoordLat': origin[0],
             'originCoordLong': origin[1],
             'destCoordLat': dest[0],
